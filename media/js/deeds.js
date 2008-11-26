dcd5a654f4e7a89c9493eebeba027fd2b7c52c98
William Amberg <wamberg@technushi.com>
2008-11-25 19:34:10 -0500

    Added POC POST javascript.

--- media/js/deeds.js
+++ media/js/deeds.js
@@ -5,9 +5,10 @@ YAHOO.util.Event.addListener(window, "load", function() {
         myDataSource.connXhrMode = "queueRequests";
         myDataSource.responseSchema = {resultsList: 'Results',
 				fields: ["fields.speaker", "fields.text", "fields.deed_date"]};
-		myDataSource.doBeforeParseData = function (oRequest, oFullResponse, oCallback) {
-			oFullResponse = {"Results": oFullResponse };
-			return oFullResponse;
+		myDataSource.doBeforeParseData = function (oRequest,
+			oFullResponse, oCallback) {
+				oFullResponse = {"Results": oFullResponse };
+				return oFullResponse;
 		};
 
         var myColumnDefs = [
@@ -30,7 +31,8 @@ YAHOO.util.Event.addListener(window, "load", function() {
             this.onDataReturnAppendRows.apply(this,arguments);
         };
         var myFailureHandler = function() {
-            this.showTableMessage(YAHOO.widget.DataTable.MSG_ERROR, YAHOO.widget.DataTable.CLASS_ERROR);
+            this.showTableMessage(YAHOO.widget.DataTable.MSG_ERROR,
+					YAHOO.widget.DataTable.CLASS_ERROR);
             this.onDataReturnAppendRows.apply(this,arguments);
         };
         var callbackObj = {
@@ -44,3 +46,40 @@ YAHOO.util.Event.addListener(window, "load", function() {
         };
     }();
 });
+
+// POST form
+var handleSuccess = function(o) {
+	var div = document.getElementById("post_status");
+	div.innerHTML = o.responseText;
+};
+
+var handleFailure = function(o) {
+	var div = document.getElementById("post_status");
+	div.innerHTML = o.responseText;
+};
+
+var callback = {
+	success: handleSuccess,
+	failure: handleFailure,
+};
+
+var purl = "/valhalla/api/json/deeds/";
+
+var makeRequest = function() {
+	var speaker = document.getElementById("speaker");
+	var text = document.getElementById("text");
+	var datetime = document.getElementById("date");
+	var newDeed = [
+		{"pk": 1, "model": "valhalla.deed", "fields": {
+			"create_date": "2008-11-24 21:22:45",
+			"text": text.value,
+			"deed_date": datetime.value,
+			"speaker": speaker.value,
+			"user": 1,
+			"witness": "web"}
+		}
+	];
+
+	var postJson = YAHOO.lang.JSON.stringify(newDeed);
+	var request = YAHOO.util.Connect.asyncRequest('POST', purl, callback, postJson);
+};
